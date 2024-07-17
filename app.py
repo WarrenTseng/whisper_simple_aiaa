e83abfe9dea40a56194b7e41eb00e33f5eec46e3
WarrenTseng <r05631018@ntu.edu.tw>
2024-07-17 19:56:46 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from whisper_aiaa import WhisperAIAA
+import os
+import time
+
+app = Flask(__name__)
+
+wa = WhisperAIAA()
+audio_path = ''
+text = ''
+text_input = 'Enter text here'
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    text_display_1 = ''
+    text_display_2 = ''
+    global audio_path
+    global text
+    global text_input
+    if request.method == 'POST':
+        text_input = request.form['text_input']
+        if 'next' in request.form:
+            audio_path, text = wa._next()
+            text_display_1 = audio_path
+            text_display_2 = text
+            text_input = text_display_2
+            cmd = 'copy '+audio_path+' static\\audio.wav'
+            os.system(cmd)
+            time.sleep(0.2)
+        elif 'annotate' in request.form:
+            wa.annotate(text_input, audio_path)
+    return render_template('index.html', text_display_1=text_display_1, text_display_2=text_display_2, text_input=text_input)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- whisper_aiaa.py
+++ whisper_aiaa.py
@@ -1,8 +1,9 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import os
 
 class WhisperAIAA:
-    def __init__(self, model_id='openai/whisper-large-v3'):
+    def __init__(self, model_id='openai/whisper-large-v3', audio_folder=None):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
@@ -25,7 +26,18 @@ class WhisperAIAA:
             torch_dtype=torch_dtype,
             device=self.device,
         )
+        if audio_folder:
+            self.audio_folder = audio_folder
+            self.audio_datalist = [f for f in os.listdir(audio_folder) if '.wav' in f or 'm4a' in f]
+            self.f_count = 0
         
+    def _next(self):
+        audio_file = self.audio_datalist[self.f_count]
+        audio_path = os.path.join(self.audio_folder, audio_file)
+        self.f_count += 1
+        text = self.inference(audio_path)
+        return audio_path, text
+    
     def inference(self, audio_path):
         return self.pipe(audio_path)['text']
     

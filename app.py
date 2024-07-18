c6cc1bbfc59c321fd59c5ba6afa90ee791d81991
WarrenTseng <r05631018@ntu.edu.tw>
2024-07-18 08:44:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import time
 app = Flask(__name__)
 
 wa = WhisperAIAA(audio_folder="dataset")
+cmd = 'cp -r dataset static/'
+os.system(cmd)
 audio_path = ''
 text = ''
 text_input = 'Enter text here'
@@ -25,12 +27,13 @@ def index():
             text_display_1 = audio_path
             text_display_2 = text
             text_input = text_display_2
-            cmd = 'cp '+audio_path+' static//audio.wav'
-            os.system(cmd)
+            # cmd = 'cp '+audio_path+' static/audio.wav'
+            # cmd = 'cp '+audio_path+' static/'
+            # os.system(cmd)
             time.sleep(0.2)
         elif 'annotate' in request.form:
             wa.annotate(text_input, audio_path)
-    return render_template('index.html', text_display_1=text_display_1, text_display_2=text_display_2, text_input=text_input)
+    return render_template('index.html', text_display_1=text_display_1, text_display_2=text_display_2, text_input=text_input, audio_path=audio_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
